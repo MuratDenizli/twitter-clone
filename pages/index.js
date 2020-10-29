f1967530f3b924bf2a90a08a07fe863d0af91c84
Murat Denizli <muratdenizli@gmail.com>
2020-10-29 21:22:00 +0300

    initial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '../components/Button';
+function HomePage() {
+    return <div>
+        <h1>Welcome to Next</h1>
+        <Button>Merhaba</Button>
+    </div>
+}
+
+export default HomePage
