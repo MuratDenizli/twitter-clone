999cf435dcd2ed53c7926a4b5fd6678068aa10bf
Murat Denizli <muratdenizli@gmail.com>
2020-11-02 13:43:48 +0300

    commit

--- components/button.js
+++ components/button.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import styles from './button.module.css';
+import React from 'react'
 
-function Button({ children }) {
-    return <button type="button" className={styles.button}>{children}</button>
+import styles from './button.module.css'
+
+function Button({ children, ...props }) {
+  return (
+    <button type="button" className={styles.button} {...props}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
--- stories/2-Icons.stories.js
+++ stories/2-Icons.stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import * as Icons from '../components/icons'
 
 export default {
