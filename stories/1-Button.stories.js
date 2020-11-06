369f5f68c4638616c5435b8dea8d65b5fa4a6678
Murat Denizli <muratdenizli@gmail.com>
2020-11-06 13:42:28 +0300

    notify,title-bold componenti geliştirildi

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Button from '../components/button'
 import NavigationButton from '../components/navigation-button'
+import { Home } from '../components/icons'
+import TitleBold from '../components/title-bold'
 import Navigation from '../components/navigation'
 
 export default {
@@ -10,6 +12,11 @@ export default {
 
 export const Normal = () => <Button>Save</Button>
 
-export const NavButton = () => <NavigationButton>Save</NavigationButton>
+export const NavButton = () => (
+  <NavigationButton>
+    <Home />
+    <TitleBold>Home</TitleBold>
+  </NavigationButton>
+)
 
-export const Nav = () => <Navigation />
+export const Nav = () => <Navigation selectedKey="home" />
