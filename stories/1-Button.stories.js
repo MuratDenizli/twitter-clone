a35b32b887affce1ada1908d4ac5838933825daf
Murat Denizli <muratdenizli@gmail.com>
2020-11-09 00:20:33 +0300

    theme button component geliştirildi

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -3,19 +3,29 @@ import React from 'react'
 import Button from '../components/button'
 import NavigationButton from '../components/navigation-button'
 import { Home } from '../components/icons'
-import TitleBold from '../components/title-bold'
+import TextTtle from '../components/text-title'
 import Navigation from '../components/navigation'
+import ThemeButton from '../components/theme-button'
 
 export default {
   title: 'Buttons'
 }
 
 export const Normal = () => <Button>Save</Button>
+export const Theme = () => (
+  <div>
+    <ThemeButton>Save</ThemeButton>
+    <ThemeButton full>Save Full</ThemeButton>
+    <ThemeButton full big>
+      Save Full
+    </ThemeButton>
+  </div>
+)
 
 export const NavButton = () => (
   <NavigationButton>
     <Home />
-    <TitleBold>Home</TitleBold>
+    <TextTtle>Home</TextTtle>
   </NavigationButton>
 )
 
