f5a61880a295d73c1d03be8e08e5ee4c5c772b12
DTMDENIZLI <murat.denizli@digiturk.com.tr>
2020-11-12 13:35:17 +0300

    postcss-custom-media plugin eklendi

--- components/col-sidebar.js
+++ components/col-sidebar.js
@@ -2,8 +2,20 @@ import React from 'react'
 import cn from 'classnames'
 import styles from './col-sidebar.module.css'
 
-function Sidebar({ children }) {
-  return <div className={cn(styles.sidebar)}>{children}</div>
+import Navigation from '../components/navigation'
+import ThemeButton from '../components/theme-button'
+import ProfileBox from '../components/profile-box'
+
+function Sidebar({ flat }) {
+  return (
+    <div className={cn(styles.sidebar)}>
+      <Navigation flat={flat} />
+      <ThemeButton big full>
+        Tweet
+      </ThemeButton>
+      <ProfileBox />
+    </div>
+  )
 }
 
 export default Sidebar
