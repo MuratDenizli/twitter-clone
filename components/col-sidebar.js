92457e049d921bd8c24fa4261237c9689a0e7d0d
DTMDENIZLI <murat.denizli@digiturk.com.tr>
2020-11-12 12:18:11 +0300

    layout tasarımı devam ediyor

---
+++ components/col-sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import cn from 'classnames'
+import styles from './col-sidebar.module.css'
+
+function Sidebar({ children }) {
+  return <div className={cn(styles.sidebar)}>{children}</div>
+}
+
+export default Sidebar
